14fa5c2506df74b2408b9d382b540fab6f0a8b93
Mohammad Hosein <mohammadhuseynamini@gmail.com>
2024-04-19 18:46:53 +0330

    fixing login signup pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,31 @@
-import 'package:blink/pages/Login.dart';
 import 'package:blink/pages/Signup.dart';
 import 'package:flutter/material.dart';
 
-import 'pages/Home.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'blink',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'بلینک!'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
 
-  final String title;
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  bool logged = false;
-
   @override
   Widget build(BuildContext context) {
-    return logged? Scaffold() : Login();
+    return Signup();
   }
 }
