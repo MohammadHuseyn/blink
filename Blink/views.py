6207d25c3818c14b5e113cca2001507bf83cb92e
Amir Ali <87865758+AmirAliAhmadi10@users.noreply.github.com>
2024-04-24 14:26:28 +0330

    Add files via upload

---
+++ Blink/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .utils import get_all_customers
+
+def users_list_view(request):
+    customers = get_all_customers()
+    context = {'Customers': customers}
+    print(context)
+    return render(request, 'customers_list.html', context)
