04812c42aad984c4d67411219c9522fc04daca33
Mohammad Hosein <mohammadhuseynamini@gmail.com>
2024-04-25 19:06:15 +0330

    completing bottom sheet for add/editing addresses

--- lib/pages/Address.dart
+++ lib/pages/Address.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class Address extends StatefulWidget {
   const Address({super.key});
@@ -15,6 +17,16 @@ class _AddressState extends State<Address> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          bottomsheed("آدرس جدید");
+        },
+        child: Icon(
+          Icons.add,
+          size: 40,
+        ),
+        backgroundColor: Color(0xFF256F46),
+      ),
       appBar: AppBar(
         centerTitle: true,
         title: Text(
@@ -23,9 +35,9 @@ class _AddressState extends State<Address> {
         ),
         backgroundColor: Color(0xFF256F46),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
           child: Column(
             children: [
               Padding(
@@ -56,14 +68,16 @@ class _AddressState extends State<Address> {
                       height: 120,
                       // color: Colors.red,
                       decoration: BoxDecoration(
-                          color: selected == 0 ? Color(0xFF2E8B57) : Colors.white,
+                          color:
+                              selected == 0 ? Color(0xFF2E8B57) : Colors.white,
                           borderRadius: BorderRadius.all(Radius.circular(25)),
                           boxShadow: [
                             BoxShadow(
                               color: Colors.grey.withOpacity(0.5),
                               spreadRadius: 5,
                               blurRadius: 7,
-                              offset: Offset(0, 3), // changes position of shadow
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
                             ),
                           ]),
                       child: Center(
@@ -111,7 +125,8 @@ class _AddressState extends State<Address> {
                               color: Colors.grey.withOpacity(0.5),
                               spreadRadius: 5,
                               blurRadius: 7,
-                              offset: Offset(0, 3), // changes position of shadow
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
                             ),
                           ]),
                       child: Center(
@@ -159,14 +174,15 @@ class _AddressState extends State<Address> {
                               color: Colors.grey.withOpacity(0.5),
                               spreadRadius: 5,
                               blurRadius: 7,
-                              offset: Offset(0, 3), // changes position of shadow
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
                             ),
                           ]),
                       child: Center(
                           child: Text(
-                            "آدرس ۲",
-                            style: TextStyle(fontSize: 30, color: Colors.black),
-                          )),
+                        "آدرس ۲",
+                        style: TextStyle(fontSize: 30, color: Colors.black),
+                      )),
                     ),
                     Expanded(child: Container())
                   ],
@@ -207,14 +223,15 @@ class _AddressState extends State<Address> {
                               color: Colors.grey.withOpacity(0.5),
                               spreadRadius: 5,
                               blurRadius: 7,
-                              offset: Offset(0, 3), // changes position of shadow
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
                             ),
                           ]),
                       child: Center(
                           child: Text(
-                            "آدرس ۲",
-                            style: TextStyle(fontSize: 30, color: Colors.black),
-                          )),
+                        "آدرس ۲",
+                        style: TextStyle(fontSize: 30, color: Colors.black),
+                      )),
                     ),
                     Expanded(child: Container())
                   ],
@@ -255,14 +272,15 @@ class _AddressState extends State<Address> {
                               color: Colors.grey.withOpacity(0.5),
                               spreadRadius: 5,
                               blurRadius: 7,
-                              offset: Offset(0, 3), // changes position of shadow
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
                             ),
                           ]),
                       child: Center(
                           child: Text(
-                            "آدرس ۲",
-                            style: TextStyle(fontSize: 30, color: Colors.black),
-                          )),
+                        "آدرس ۲",
+                        style: TextStyle(fontSize: 30, color: Colors.black),
+                      )),
                     ),
                     Expanded(child: Container())
                   ],
@@ -303,14 +321,15 @@ class _AddressState extends State<Address> {
                               color: Colors.grey.withOpacity(0.5),
                               spreadRadius: 5,
                               blurRadius: 7,
-                              offset: Offset(0, 3), // changes position of shadow
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
                             ),
                           ]),
                       child: Center(
                           child: Text(
-                            "آدرس ۲",
-                            style: TextStyle(fontSize: 30, color: Colors.black),
-                          )),
+                        "آدرس ۲",
+                        style: TextStyle(fontSize: 30, color: Colors.black),
+                      )),
                     ),
                     Expanded(child: Container())
                   ],
@@ -322,4 +341,110 @@ class _AddressState extends State<Address> {
       ),
     );
   }
+
+  void bottomsheed(title) {
+    showModalBottomSheet(
+        context: context,
+        builder: (builder) {
+          return Scaffold(
+            body: Column(children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 20, right: 20, left: 20, bottom: 20),
+                child: Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: Theme(
+                      data: ThemeData(
+                        primaryColor: Colors.redAccent,
+                        primaryColorDark: Colors.red,
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.teal),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          labelText: 'نام آدرس',
+                          floatingLabelStyle: TextStyle(fontSize: 25),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          labelStyle: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                    )),
+              ),
+              Container(
+                height: MediaQuery.of(builder).size.height * 0.30,
+                child: Scaffold(
+                  body: map(),
+                ),
+              )
+            ]),
+            bottomNavigationBar: Container(
+              // color: Colors.red,
+              height: MediaQuery.of(builder).size.height * 0.085,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1,1),
+                    blurRadius: 10,
+                    spreadRadius: 5
+                  )
+                ]
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child:Text("افزودن آدرس",
+                  style: TextStyle(
+                    fontSize: 25
+                  ),),
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                          ),
+                      ),
+                    backgroundColor: MaterialStateColor.resolveWith((states) => Color(0xFF256F46))
+                  ),
+                ),
+              ),
+            )
+          );
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+        ));
+  }
+
+  Widget map() {
+    return FlutterMap(
+        options: MapOptions(
+            initialCenter: LatLng(35.715298, 51.404343),
+            initialZoom: 11,
+            interactionOptions:
+                InteractionOptions(flags: ~InteractiveFlag.doubleTapZoom)),
+        children: [
+          tilelayer,
+          MarkerLayer(markers: [
+            // Marker(point: point, child: child)
+          ])
+        ]);
+  }
 }
+
+TileLayer get tilelayer => TileLayer(
+      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      userAgentPackageName: 'com.blink.example',
+    );
