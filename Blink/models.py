c519d7e7056383c8f8f164f06ceb9c45f771e602
Artin Elhami <Artinelhamirad@gmail.com>
2024-04-25 10:41:42 +0330

    discount code implementation

--- Blink/models.py
+++ Blink/models.py
@@ -4,6 +4,9 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.utils.translation import gettext_lazy as _
 import json
 from django.contrib.auth.hashers import check_password
+import random
+
+
 class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     firstname = models.CharField(max_length=100)
@@ -62,10 +65,13 @@ class User(models.Model):
             print(f"An error occurred: {e}")
             return 0
 
+
 class Customer(User):
     location = models.ForeignKey("Location", related_name='customers', on_delete=models.SET_NULL, null=True, blank=True)
+
     def __str__(self):
         return self.username
+
 
 class Delivery(User):
     driving_license_number = models.CharField(max_length=20, unique=True, help_text="Unique driving license number")
@@ -74,13 +80,16 @@ class Delivery(User):
     def __str__(self):
         return f"{self.username} (Driving License: {self.driving_license_number})"
 
+
 class Administrator(User):
     def __str__(self):
         return self.username
 
+
 class CustomerSupport(User):
     def __str__(self):
         return self.username
+
 
 class Location(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
@@ -95,6 +104,7 @@ class Location(models.Model):
 class Seller(User):
     store = models.OneToOneField('Store', related_name='seller_profile', on_delete=models.CASCADE)
 
+
 class DiscountCode(models.Model):
     code = models.CharField(max_length=20, unique=True)
     discount_value = models.IntegerField(default=0, help_text="Discount value between 1 to 100")
@@ -103,6 +113,7 @@ class DiscountCode(models.Model):
 
     def __str__(self):
         return self.code
+
 
 class Store(models.Model):
     name = models.CharField(max_length=100)
@@ -113,12 +124,12 @@ class Store(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     store = models.ForeignKey(Store, related_name='products', on_delete=models.CASCADE, null=True)
-
 
     def __str__(self):
         return self.name
@@ -131,6 +142,7 @@ class ProductPicture(models.Model):
     def __str__(self):
         return f"{self.product.name} Picture"
 
+
 class ProductComment(models.Model):
     product = models.ForeignKey(Product, related_name='comments', on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, related_name='comments', on_delete=models.CASCADE)
@@ -142,6 +154,7 @@ class ProductComment(models.Model):
     def __str__(self):
         return f"Comment for {self.product.name}"
 
+
 class SalesReport(models.Model):
     store = models.ForeignKey(Store, related_name='sales_reports', on_delete=models.CASCADE)
     date = models.DateField()
@@ -151,12 +164,14 @@ class SalesReport(models.Model):
     def __str__(self):
         return f"Sales Report for {self.store.name} on {self.date}"
 
+
 class Chat(models.Model):
     customer_support = models.ForeignKey(CustomerSupport, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Chat between {self.customer_support.user.username} and {self.customer.username}"
+
 
 class Message(models.Model):
     chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE)
@@ -168,6 +183,7 @@ class Message(models.Model):
 
     def __str__(self):
         return f"Message in chat between {self.chat.customer_support.user.username} and {self.chat.customer.username}"
+
 
 class Order(models.Model):
     # Define order status choices
@@ -181,10 +197,13 @@ class Order(models.Model):
         FAILED = 'FAILED', _('Failed')
 
     customer = models.ForeignKey(Customer, related_name='orders', on_delete=models.CASCADE)
-    delivery_location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
-    delivery_person = models.ForeignKey(Delivery, related_name='orders', on_delete=models.SET_NULL, null=True, blank=True)
+    delivery_location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True,
+                                          related_name='orders')
+    delivery_person = models.ForeignKey(Delivery, related_name='orders', on_delete=models.SET_NULL, null=True,
+                                        blank=True)
     delivery_price = models.DecimalField(max_digits=6, decimal_places=2, help_text="Cost of delivery")
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Total price of the order, including items and delivery")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2,
+                                      help_text="Total price of the order, including items and delivery")
     status = models.CharField(
         max_length=10,
         choices=OrderStatus.choices,
@@ -197,6 +216,7 @@ class Order(models.Model):
     def __str__(self):
         return f"Order #{self.id} by {self.customer.username} - Total: {self.total_price} - Status: {self.status}"
 
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
@@ -204,3 +224,25 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.product.name} (x{self.quantity}) in Order #{self.order.id}"
+
+
+class DiscountCode(models.Model):
+    code = models.CharField(max_length=8, unique=True)
+    is_used = models.BooleanField(default=False)
+
+    def generate_code(self):
+        self.code = ''.join(random.choices('0123456789', k=8))
+
+    def check_and_use_code(self, input_code):
+        if input_code == self.code and not self.is_used:
+            self.is_used = True
+            self.save()
+            return True
+        else:
+            return False
+
+    def uncheck_code(self, input_code):
+        if input_code == self.code and self.is_used:
+            self.is_used = False
+            self.save()
+
