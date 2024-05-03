1625872dd0fbebe4e28b4337b62149faf16b81eb
Artin Elhami <Artinelhamirad@gmail.com>
2024-05-03 13:13:55 +0330

    Convert to REST

--- Blink/views.py
+++ Blink/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from .utils import get_all_customers
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework import permissions, viewsets
+from .serialaizers import UserSerializer
+from rest_framework.views import APIView
 
-def users_list_view(request):
-    customers = get_all_customers()
 
-    context = {'Customers': customers}
-
-    return render(request, 'customers_list.html', context)
+class UserList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    def post(self, request,):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,)
+        return Response(serializer.errors,)
