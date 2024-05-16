e07e95d73dfaf32f4d9de53819f1c6b824318bbe
Mohammad Hosein <mohammadhuseynamini@gmail.com>
2024-05-17 03:20:22 +0330

    product detail page design finished

--- lib/pages/StorePage.dart
+++ lib/pages/StorePage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:blink/pages/Home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -122,10 +124,14 @@ class _StorePageState extends State<StorePage> {
                           scrollDirection: Axis.vertical,
                           itemCount: store.items.length,
                           itemBuilder: (cntx, i) {
-                            return Row(children: [
-                              prodcut(15, 0, store.items[i]),
-                              i + 1 >= store.items.length? Container() : prodcut(15, 0, store.items[i + 1]),
-                            ],);
+                            return Row(
+                              children: [
+                                prodcut(15, 0, store.items[i]),
+                                i + 1 >= store.items.length
+                                    ? Container()
+                                    : prodcut(15, 0, store.items[i + 1]),
+                              ],
+                            );
                           }),
                     )
                   ],
@@ -133,7 +139,9 @@ class _StorePageState extends State<StorePage> {
               ),
               decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50)),
                   boxShadow: [
                     BoxShadow(
                         color: Colors.grey,
@@ -173,7 +181,7 @@ class _StorePageState extends State<StorePage> {
       padding: EdgeInsets.only(left: left, right: right, top: 20),
       child: GestureDetector(
         onTap: () {
-          bottomShett();
+          bottomShett(item);
         },
         child: Container(
           width: MediaQuery.of(context).size.width * 0.39,
@@ -249,8 +257,10 @@ class _StorePageState extends State<StorePage> {
                         padding: const EdgeInsets.only(bottom: 10),
                         child: IconButton(
                             onPressed: () {
-                              item.count += 1;
-                              _add_to_cart(item);
+                              setState(() {
+                                item.count += 1;
+                                _add_to_cart(item);
+                              });
                             },
                             icon: ImageIcon(AssetImage("images/plus.png"),
                                 color: Color(0xFF2E8B57))),
@@ -258,7 +268,10 @@ class _StorePageState extends State<StorePage> {
               ),
               Padding(
                 padding: const EdgeInsets.only(bottom: 10),
-                child: Text(item.name, style: TextStyle(fontSize: 23),),
+                child: Text(
+                  item.name,
+                  style: TextStyle(fontSize: 23),
+                ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -308,7 +321,234 @@ class _StorePageState extends State<StorePage> {
     });
   }
 
-  void bottomShett() {
-
+  void bottomShett(Item item) {
+    showModalBottomSheet(
+        context: context,
+        backgroundColor: Colors.transparent,
+        builder: (builder) {
+          return StatefulBuilder(
+            builder: (context, StateSetter setState) {
+              return Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(25)),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 25),
+                        child: Container(
+                            width: MediaQuery
+                                .of(context)
+                                .size
+                                .width * 0.85,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                color: Color(0xFFEAF3EE)
+                              // color: Colors.red
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 35),
+                              child: ImageIcon(
+                                AssetImage("images/product.png"),
+                                color: Color(0xFF5E846E),
+                                size: 95,
+                              ),
+                            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.star_rate_rounded,
+                              size: 32,
+                              color: Color(0xFF256f46),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5),
+                              child: Text(
+                                "4.1/5",
+                                style: TextStyle(fontSize: 17),
+                              ),
+                            ),
+                            Expanded(child: Container()),
+                            Text(
+                              "نام محصول",
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: Row(
+                          children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.arrow_back_ios_rounded)),
+                            Text(
+                              "مشاهده نظرات",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Expanded(child: Container()),
+                            Text(
+                              "نام فروشگاه",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 15),
+                              child: ImageIcon(
+                                AssetImage("images/shop.png"),
+                                size: 50,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 200,
+                        width: MediaQuery
+                            .of(context)
+                            .size
+                            .width * 0.85,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color(0xFF518C6B),
+                                  spreadRadius: 0,
+                                  blurRadius: 0,
+                                  offset: Offset(0, -2)),
+                              BoxShadow(
+                                  color: Color(0xFF518C6B),
+                                  spreadRadius: 0,
+                                  blurRadius: 0,
+                                  offset: Offset(0, 2))
+                            ],
+                            color: Colors.white,
+                            // color: Colors.red,
+                            borderRadius: BorderRadius.circular(35)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 25, vertical: 25),
+                          child: Text(
+                            "توضیحات",
+                            textDirection: TextDirection.rtl,
+                            style: TextStyle(fontSize: 20, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                        const EdgeInsets.only(left: 35, right: 35, top: 20),
+                        child: Row(
+                            children: [
+                              Text("تومان ",
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 22,
+                                      fontFamily: 'shabnam')),
+                              Text(item.price.toString(),
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontFamily: 'shabnam',
+                                      color: Colors.black)),
+                              Expanded(child: Container()),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 15),
+                                child: global.card.contains(item)
+                                    ? Padding(
+                                  padding: const EdgeInsets.only(bottom: 10),
+                                  child: Row(
+                                    children: [
+                                      IconButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              item.count -= 1;
+                                            });
+                                            if (item.count == 0) {
+                                              _remove_from_cart(item);
+                                            }
+                                          },
+                                          icon: ImageIcon(
+                                              AssetImage(
+                                                "images/remove.png",
+                                              ),
+                                              color: Colors.red
+                                            // color: Colors.black,
+                                          )),
+                                      // Expanded(child: Container()),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                                        child: Text(
+                                          item.count.toString(),
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      ),
+                                      // Expanded(child: Container()),
+                                      IconButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              item.count += 1;
+                                            });
+                                          },
+                                          icon: ImageIcon(
+                                            AssetImage("images/plus.png"),
+                                            color: Color(0xFF2E8B57),
+                                          ))
+                                    ],
+                                  ),
+                                )
+                                    : ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        item.count += 1;
+                                        _add_to_cart(item);
+                                      });
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(vertical: 5),
+                                      child: Text(
+                                        "  افزودن  ",
+                                        style: TextStyle(
+                                            fontSize: 25, color: Color(0xFF256F46)),
+                                      ),
+                                    ),
+                                    style: ButtonStyle(
+                                        backgroundColor: MaterialStateColor.resolveWith((states) => Colors.white),
+                                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(18.0),
+                                                side: BorderSide(color: Color(0xFF256F46),width: 2)
+                                            )
+                                        )
+                                    )
+                                ),
+                              ),
+                            ]),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }
+          );
+        });
+  }
+  void add_count(Item item) {
+    setState(() {
+      item.count += 1;
+    });
+  }
+  void minus_count(Item item) {
+    setState(() {
+      item.count -= 1;
+    });
   }
 }
