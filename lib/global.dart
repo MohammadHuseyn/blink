78424207ed7491a6ff4a751bccb99c8d27232739
Mohammad Hosein <mohammadhuseynamini@gmail.com>
2024-05-26 00:08:54 +0330

    setting backend server ip

--- lib/global.dart
+++ lib/global.dart
@@ -8,8 +8,8 @@ import 'classes/item.dart';
 // var url ='http://10.0.2.2:8000';
 // var url ='http://192.168.1.3:8000';
 // committing
-var url ='http://192.168.124.45:8000';
-// var url = 'http://62.60.205.58:8001';
+// var url ='http://192.168.124.45:8000';
+var url = 'http://62.60.205.58:8000';
 
 
 
